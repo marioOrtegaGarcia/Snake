800f0a34a539977644f1b2dab72cfedf98786ab6
marioOrtegaGarcia <mario.j.ortega.garcia@gmail.com>
2018-05-07 15:51:09 -0700

    comments out AL's collision check

--- Snake.cpp
+++ Snake.cpp
@@ -78,7 +78,7 @@ void Snake::collisionCheck() {
     float y = snake.front()->y;
     if (x > 1.0 - girth || x < -1+girth || y > 0.83-girth || y < -1+girth) {
         alive = false;
-    }
+    }/*
     for (std::list<Coord*>::iterator i = snake.begin(); i != snake.end(); ++i) {
         if (i == snake.begin()) {
             for (int it = 0; it < 7; it++)
@@ -87,7 +87,7 @@ void Snake::collisionCheck() {
         else if ((x > (*i)->x && x < (*i)->x + girth) && (y < (*i)->y && y > (*i)->y - girth)) {
             alive = false;
         }
-    }
+    }*/
 }
 void Snake::draw() {
     list<Coord*>::iterator itr;
